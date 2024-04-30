d05873e5ee27012551d5a72bab48cdf9d2f56bd9
Hagarsultan88 <97818325+Hagarsultan88@users.noreply.github.com>
2024-04-30 05:13:04 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import Accordian from "./Components/accordian";
+import "./App.css";
+
+function App() {
+    return (
+        <div className="App">
+            <Accordian/>
+        </div>
+    );
+}
+
+export default App;
